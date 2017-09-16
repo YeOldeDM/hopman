f01267cd5b12f7054ed8b91d445b97c89d171d03
YeOldeDM <nines@email.com>
2017-09-16 17:47:34 -0600

    Dogio changes facing when running

--- Mario/Dogio/Dogio.gd
+++ Mario/Dogio/Dogio.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
+onready var sprite = get_node("Sprite")
+
 var vel = Vector2()
 var run_speed = 150
 
-
+var facing = 1 setget _set_facing
 
 func _ready():
 	set_fixed_process( true )
@@ -12,8 +14,22 @@ func _ready():
 func _fixed_process( delta ):
 	var LEFT = Input.is_action_pressed( "LEFT" )
 	var RIGHT = Input.is_action_pressed( "RIGHT" )
-	
+	var new_facing = facing
 	vel.x = (RIGHT - LEFT) * run_speed * delta
 	
+
 	var motion = move( vel )
 	
+	if LEFT or RIGHT:
+		new_facing = sign( vel.x )
+	
+	if new_facing != self.facing:
+		self.facing = new_facing
+
+
+
+func _set_facing( what ):
+	facing = what
+	sprite.set_scale( Vector2( facing, 1 ) )
+
+
